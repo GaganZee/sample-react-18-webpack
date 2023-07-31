0acef4a8af48ef585cad1ded3887dd6e83fda07f
GaganZee <gagan.h@zee.com>
2023-07-31 13:50:37 +0530

    feat: update react-redux package

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   devtool: "inline-source-map",
   output: {
     path: path.join(__dirname, "/dist"),
